290788264f0aedc19e30263fd71a0d5f80a96b46
Maks Kolman <maks@kolman.si>
2022-12-18 13:03:28 +0100

    Add another test and test examples individually

--- src/test.rs
+++ src/test.rs
@@ -2,19 +2,42 @@ use std::{collections::HashMap, fs};
 
 use crate::runner::interpret;
 
+macro_rules! interpret_tests {
+    ($($name:ident,)*) => {
+    $(
+        #[test]
+        fn $name() {
+            run_single_example(stringify!($name));
+        }
+    )*
+    }
+}
+
 #[test]
 fn test_examples() {
     for (code_file, out_file) in collect_examples() {
-        let code = fs::read_to_string(&code_file).expect("Invalid code file");
-        let want = fs::read_to_string(&out_file).expect("Invalid out file");
-        assert_eq!(
-            interpret(&code),
-            Ok(want),
-            "\n\tInvalid result for {} in {}",
-            code_file,
-            out_file,
-        );
+        run_and_compare(&code_file, &out_file);
     }
+}
+
+fn run_single_example(test_case: &str) {
+    run_and_compare(
+        &format!("./examples/{}.aoc", test_case),
+        &format!("./examples/{}.out", test_case),
+    )
+}
+
+fn run_and_compare(code_file: &str, out_file: &str) {
+    println!("TESTING {} AND {}", code_file, out_file);
+    let code = fs::read_to_string(code_file).expect("Invalid code file");
+    let want = fs::read_to_string(out_file).expect("Invalid out file");
+    assert_eq!(
+        interpret(&code),
+        Ok(want),
+        "\n\tInvalid result for {} in {}",
+        code_file,
+        out_file,
+    );
 }
 
 fn collect_examples() -> Vec<(String, String)> {
@@ -29,4 +52,10 @@ fn collect_examples() -> Vec<(String, String)> {
         }
     }
     result.values().cloned().collect()
+}
+
+interpret_tests! {
+    primes,
+    sort,
+    dfs,
 }
